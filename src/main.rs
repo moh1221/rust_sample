05dc37dc79bc96c5ce8359f02a903a4e81abefba
moh1221 <moh_abo@hotmail.com>
2020-07-12 19:41:03 -0400

    Update Order Struct

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 mod order;
 
 fn main(){
-    let d: order::Order = order::Order::new(String::from("first"), String::from("order # 1"), 20.0);
+    let d: order::Order = order::Order::new(String::from("first"), 2, String::from("order # 1"), 20.0);
     println!("{}", d);
 }
--- src/order.rs
+++ src/order.rs
@@ -2,14 +2,16 @@ use std::fmt;
 
 pub struct Order {
     name: String,
+    level: i32,
     des: String,
     val: f32
 }
 
 impl Order {
-    pub fn new(name: String, des: String, val: f32) -> Order {
+    pub fn new(name: String, level: i32, des: String, val: f32) -> Order {
         Order {
             name,
+            level,
             des,
             val
         }
@@ -18,6 +20,6 @@ impl Order {
 
 impl fmt::Display for Order {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> Result<(), fmt::Error> { 
-        write!(f, "Order: {} '{}' @ {}", self.name, self.des, self.val)
+        write!(f, "Order: {} '{}' @ {} - level {}", self.name, self.des, self.val, self.level)
     }
 }
